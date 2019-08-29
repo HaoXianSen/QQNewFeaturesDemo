3e4db8b3c7a564a24a7d129d896fb2d8c08c25a2
haoyuhong <haoyuhong@dangdang.com>
2019-08-29 10:23:53 +0800

    初始化工程

---
+++ QQNewFeaturesDemo/QQNewFeaturesDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  QQNewFeaturesDemo
+//
+//  Created by harry on 2019/8/29.
+//  Copyright © 2019 DangDang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
